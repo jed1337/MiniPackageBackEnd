b51cc62dc15f1a33a3ebbaa1c42537f1bcd35cfd
JEDIDIAH YSAMM CAYCHINGCO (SHP-IRIS-ISD-OOCLL/MNL) <CAYCHJE@OOCL.COM>
2019-10-24 10:44:16 +0800

    Add find and post tests

--- src/main/java/com/tw/apistackbase/controller/PickupController.java
+++ src/main/java/com/tw/apistackbase/controller/PickupController.java
@@ -4,6 +4,8 @@ import com.tw.apistackbase.entity.Pickup;
 import com.tw.apistackbase.entityBasis.Company;
 import com.tw.apistackbase.services.PickupService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -14,9 +16,16 @@ public class PickupController {
     @Autowired
     private PickupService pickupService;
 
-    @GetMapping(produces = {"application/json"})
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(code= HttpStatus.OK)
     @RequestMapping("/{id}")
     public Pickup findPickup(@PathVariable int id){
         return pickupService.findById(id);
+    }
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(code= HttpStatus.CREATED)
+    public Pickup postNewPickup(@RequestBody Pickup newPickup){
+        return pickupService.postNewPickup(newPickup);
     }
 }
--- src/test/java/com/tw/apistackbase/controller/PickupControllerTest.java
+++ src/test/java/com/tw/apistackbase/controller/PickupControllerTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,6 +23,7 @@ import static org.mockito.Mockito.ignoreStubs;
 import static org.mockito.Mockito.when;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -48,6 +50,23 @@ public class PickupControllerTest {
         ResultActions result = mvc.perform(get("/pickups/1"));
 
         result.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.pickupNumber", is(1)))
+                .andExpect(jsonPath("$.pickupTime", is("12")));
+    }
+
+    @Test
+    public void should_post_newPickup() throws Exception {
+        Pickup newPickup = new Pickup(1);
+        newPickup.setPickupTime("12");
+        when(pickupService.postNewPickup(newPickup)).thenReturn(newPickup);
+
+        ResultActions result = mvc.perform(post("/pickups/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(newPickup))
+        );
+
+        result.andExpect(status().isCreated())
                 .andDo(print())
                 .andExpect(jsonPath("$.pickupNumber", is(1)))
                 .andExpect(jsonPath("$.pickupTime", is("12")));
